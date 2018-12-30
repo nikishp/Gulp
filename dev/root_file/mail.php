200bf9ba822fc3aca688f40bdcf6a5efe6f58eb8
nikishp <shpakovskiynl@gmail.com>
2018-12-30 17:18:48 +0200

    update

--- dev/root_file/mail.php
+++ dev/root_file/mail.php
@@ -5,8 +5,9 @@ header('Content-type: application/json');
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/OAuth.php';
 require 'PHPMailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);// Passing `true` enables exceptions
